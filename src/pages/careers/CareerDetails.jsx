75a8ed43a18838a3498bbfc76a41f60c79e0e803
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-13 13:51:12 +0100

    Route Parameters: Loader

--- src/pages/careers/CareerDetails.jsx
+++ src/pages/careers/CareerDetails.jsx
@@ -1,10 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 export default function CareerDetails() {
   const { id } = useParams();
+  const career = useLoaderData;
   return (
     <div className="career-details">
-      <h2>{id}</h2>
+      <h2>Career details for {career.title}</h2>
+      <p>Starting salary: {career.salary}</p>
+      <p>Location: {career.location}</p>
+      <div className="details">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </div>
     </div>
   );
 }
+
+// loader function
+export const careerDetailsLoader = async ({ params }) => {
+  const { id } = params;
+
+  const res = await fetch("http://localhost:4000/careers" + id);
+
+  return res.json();
+};
